1105d6174b26ca8feaa6188461d7b1f6be1658e6
iyrd <chilromko@gmail.com>
2025-03-05 15:11:53 +0100

    ddd

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -3,6 +3,7 @@ import styles from './Feedback.module.css';
 
 const Feedback = ({ feedback, totalFeedback, positivePercentage }) => {
   return (
+    
     <div className={styles.feedback}>
       <h2>Statistics</h2>
       <p>Good: {feedback.good}</p>
